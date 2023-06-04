fe2e139f36382d59221aec2d55097a4666814865
yevdem82 <evdem82@yandex.ru>
2023-06-04 16:34:43 +0300

    Add validation

--- cats/views.py
+++ cats/views.py
@@ -9,6 +9,12 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
